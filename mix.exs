f38211d4b10a8fc3b7b8762b5903b83e0f4cc58f
Siyabend Ürün <urunsiyabend@gmail.com>
2025-06-11 15:38:09 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Identicon.MixProject do
     [
       app: :identicon,
       version: "0.1.0",
-      elixir: "~> 1.12.3",
+      elixir: "~> 1.17.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
